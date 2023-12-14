8688828669ca98d9caa613cd1a21c8054bb9515f
Sean Moen <spaulm5@gmail.com>
2023-12-14 15:34:34 -0600

    Fixed error where deleted items in cart would still appear

--- cart.php
+++ cart.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<head>
-	<link rel="stylesheet" href="mobile.css" media="screen and (max-width:480px)"/>
-    <link rel="stylesheet" href="tablet.css" media="screen and (min-width:481px) and (max-width:768px)"/>
-    <link rel="stylesheet" href="desktop.css" media="screen and (min-width:769px)"/>
-    <meta charset="UTF-8" />
-</head>
-<main>
-
-    <?php
+<?php
     session_start();
     if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) &&
         is_numeric($_POST['quantity'])) {
@@ -30,43 +21,44 @@
                 $_SESSION['cart'] = array($product_id => $quantity);
             }
         }
+?>
+    <!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="mobile.css" media="screen and (max-width:480px)"/>
+        <link rel="stylesheet" href="tablet.css" media="screen and (min-width:481px) and (max-width:768px)"/>
+        <link rel="stylesheet" href="desktop.css" media="screen and (min-width:769px)"/>
+        <meta charset="UTF-8" />
+    </head>
+    <main>
+
+        <?php include_once "header.php"; 
+        require_once("functions.php");
         ?>
+        
+        <h2>Shopping Cart</h2>
 
-        <!DOCTYPE html>
-        <head>
-            <link rel="stylesheet" href="mobile.css" media="screen and (max-width:480px)"/>
-            <link rel="stylesheet" href="tablet.css" media="screen and (min-width:481px) and (max-width:768px)"/>
-            <link rel="stylesheet" href="desktop.css" media="screen and (min-width:769px)"/>
-            <meta charset="UTF-8" />
-        </head>
-        <main>
-
-            <?php include_once "header.php"; 
-            require_once("functions.php");
-            ?>
-            
-            <h2>Shopping Cart</h2>
-
-            <?php
-            $subtotal = 0.00;
-            $total = 0.00;
-            $tax = 1.07;
-            if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
-                echo '<div class="pokemon_list">';
-                foreach ($_SESSION['cart'] as $key => $quantity) {
+        <?php
+        $subtotal = 0.00;
+        $total = 0.00;
+        $tax = 1.07;
+        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            echo '<div class="pokemon_list">';
+            foreach ($_SESSION['cart'] as $key => $quantity) {
+                if ($quantity > 0) {
                     $pokemon = getPokemonByID($key);
                     echo $pokemon->getCartCard();
                     $subtotal += $pokemon->getPrice() * $quantity;
                 }
-                echo "</div>";
-            } else {
-                echo <<<POKEMON
-                <h3>No Pokemon Added to Cart</h3>
-                POKEMON;
             }
-            echo "<h3>Subtotal: " . $subtotal . "</h3>";
-            $total = $subtotal * $tax;
-            echo "<h3>Total: " . number_format((float)$total, 2, '.', '') . "</h3>";
-            ?>
+            echo "</div>";
+        } else {
+            echo <<<POKEMON
+            <h3>No Pokemon Added to Cart</h3>
+            POKEMON;
+        }
+        echo "<h3>Subtotal: " . $subtotal . "</h3>";
+        $total = $subtotal * $tax;
+        echo "<h3>Total: " . number_format((float)$total, 2, '.', '') . "</h3>";
+        ?>
 
 </main>
